ed3abf596cd397f941dbd10ebaa115188799ffbc
Kawui Kawai <kawui.kawai8@gmail.com>
2021-05-15 19:48:47 -0500

    first commit

--- server/bot.js
+++ server/bot.js
@@ -1,7 +1,7 @@
 const { Client } = require("discord.js");
 const client = new Client();
 
-const { token } = require("../config/config")
+const { token } = require("../config/config");
 
 client.on("ready", () => {
   console.log("Bot Prendido en la web");
